0920ab4c033337c2a91b7211a288d3898f669bd0
zhubicen <zhubicen@gmail.com>
2012-05-09 06:58:17 +0800

    run gofmt on listbox.go

--- listbox.go
+++ listbox.go
@@ -14,22 +14,22 @@ import . "github.com/lxn/go-winapi"
 var listBoxOrigWndProcPtr uintptr
 var _ subclassedWidget = &ListBox{}
 
-type ListBox struct{
+type ListBox struct {
 	WidgetBase
-	maxItemTextWidth              int
+	maxItemTextWidth             int
 	CurrentIndexChangedPublisher EventPublisher
-	dblClickedPublisher            EventPublisher
+	dblClickedPublisher          EventPublisher
 }
 
-func NewListBox(parent Container)(*ListBox, error){
+func NewListBox(parent Container) (*ListBox, error) {
 	lb := &ListBox{}
 	err := initChildWidget(
 		lb,
 		parent,
 		"LISTBOX",
-		WS_TABSTOP | WS_VISIBLE | LBS_STANDARD,
+		WS_TABSTOP|WS_VISIBLE|LBS_STANDARD,
 		0)
-	if err != nil{
+	if err != nil {
 		return nil, err
 	}
 	return lb, nil
@@ -47,17 +47,17 @@ func (*ListBox) LayoutFlags() LayoutFlags {
 	return GrowableHorz | GrowableVert
 }
 
-func (lb *ListBox) AddString(item string){
-	SendMessage (lb.hWnd, LB_ADDSTRING, 0, 
+func (lb *ListBox) AddString(item string) {
+	SendMessage(lb.hWnd, LB_ADDSTRING, 0,
 		uintptr(unsafe.Pointer(syscall.StringToUTF16Ptr(item))))
 }
 
 //If lb parameter is -1, the string is added to the end of the list.
-func (lb *ListBox) InsertString(index int, item string) error{
-	if index < -1{
+func (lb *ListBox) InsertString(index int, item string) error {
+	if index < -1 {
 		return newError("Invalid index")
 	}
-	
+
 	ret := int(SendMessage(lb.hWnd, LB_INSERTSTRING, uintptr(index), uintptr(unsafe.Pointer(syscall.StringToUTF16Ptr(item)))))
 	if ret == LB_ERRSPACE || ret == LB_ERR {
 		return newError("Fail to insert string")
@@ -65,7 +65,7 @@ func (lb *ListBox) InsertString(index int, item string) error{
 	return nil
 }
 
-func (lb *ListBox) DeleteString(index uint) error{
+func (lb *ListBox) DeleteString(index uint) error {
 	ret := int(SendMessage(lb.hWnd, LB_DELETESTRING, uintptr(index), 0))
 	if ret == LB_ERR {
 		return newError("Fail to delete string")
@@ -73,32 +73,31 @@ func (lb *ListBox) DeleteString(index uint) error{
 	return nil
 }
 
-func (lb *ListBox) GetString(index uint) string{
+func (lb *ListBox) GetString(index uint) string {
 	len := int(SendMessage(lb.hWnd, LB_GETTEXTLEN, uintptr(index), 0))
-	if len == LB_ERR{
+	if len == LB_ERR {
 		return ""
 	}
 
-	buf := make([]uint16, len + 1)
+	buf := make([]uint16, len+1)
 	_ = SendMessage(lb.hWnd, LB_GETTEXT, uintptr(index), uintptr(unsafe.Pointer(&buf[0])))
-	
-	if len == LB_ERR{
+
+	if len == LB_ERR {
 		return ""
 	}
 	return syscall.UTF16ToString(buf)
 }
-	
 
-func (lb *ListBox) ResetContent(){
+func (lb *ListBox) ResetContent() {
 	SendMessage(lb.hWnd, LB_RESETCONTENT, 0, 0)
 }
 
-//The return value is the number of items in the list box, 
+//The return value is the number of items in the list box,
 //or LB_ERR (-1) if an error occurs.
-func (lb *ListBox) GetCount() (uint, error){
-    retPtr := SendMessage(lb.hWnd, LB_GETCOUNT, 0, 0)
+func (lb *ListBox) GetCount() (uint, error) {
+	retPtr := SendMessage(lb.hWnd, LB_GETCOUNT, 0, 0)
 	ret := int(retPtr)
-	if ret == LB_ERR{
+	if ret == LB_ERR {
 		return 0, newError("Fail to get count")
 	}
 	return uint(ret), nil
@@ -119,8 +118,8 @@ func (lb *ListBox) calculateMaxItemTextWidth() int {
 
 	count, _ := lb.GetCount()
 	var i uint
-	for i = 0; i < count ; i++{
-		item  := lb.GetString(i)
+	for i = 0; i < count; i++ {
+		item := lb.GetString(i)
 		var s SIZE
 		str := syscall.StringToUTF16(item)
 
@@ -135,7 +134,6 @@ func (lb *ListBox) calculateMaxItemTextWidth() int {
 	return maxWidth
 }
 
-
 func (lb *ListBox) SizeHint() Size {
 
 	defaultSize := lb.dialogBaseUnitsToPixels(Size{50, 12})
@@ -148,35 +146,35 @@ func (lb *ListBox) SizeHint() Size {
 	w := maxi(defaultSize.Width, lb.maxItemTextWidth+24)
 	h := defaultSize.Height + 1
 
-	return Size{w, h}	
+	return Size{w, h}
 
 }
 
-func (lb *ListBox) CurrentIndex() int{
-	return int(SendMessage (lb.hWnd, LB_GETCURSEL, 0, 0))
+func (lb *ListBox) CurrentIndex() int {
+	return int(SendMessage(lb.hWnd, LB_GETCURSEL, 0, 0))
 }
 
-func (lb *ListBox) SetCurrentIndex(value int) error{
+func (lb *ListBox) SetCurrentIndex(value int) error {
 	ret := int(SendMessage(lb.hWnd, LB_SETCURSEL, uintptr(value), 0))
-	if ret == LB_ERR{
+	if ret == LB_ERR {
 		return newError("Invalid index or ensure lb is single-selection listbox")
 	}
 	return nil
 }
 
-func (lb *ListBox) CurrentString() string{
+func (lb *ListBox) CurrentString() string {
 	index := lb.CurrentIndex()
 	length := int(SendMessage(lb.hWnd, LB_GETTEXTLEN, uintptr(index), 0)) + 1
-	buffer := make([]uint16, length +1)
+	buffer := make([]uint16, length+1)
 	SendMessage(lb.hWnd, LB_GETTEXT, uintptr(index), uintptr(unsafe.Pointer(&buffer[0])))
 	return syscall.UTF16ToString(buffer)
 }
 
-func (lb *ListBox) CurrentIndexChanged() *Event{
+func (lb *ListBox) CurrentIndexChanged() *Event {
 	return lb.CurrentIndexChangedPublisher.Event()
 }
 
-func (lb *ListBox) DblClicked() *Event{
+func (lb *ListBox) DblClicked() *Event {
 	return lb.dblClickedPublisher.Event()
 }
 
