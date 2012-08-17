08e8e4a48a5d3bcdbafeb112f66edaef2f6c037c
Alexander Neumann <alexander.neumann@picos-software.com>
2012-08-17 15:41:06 +0200

    Add Sorter interface and SorterBase base implementation.
    The Sorter interface should be implemented by types that also implement TableModel to integrate with the sort features of widgets like TableView.

--- models.go
+++ models.go
@@ -119,3 +119,73 @@ type ItemChecker interface {
 	// SetChecked sets if the specified item is checked.
 	SetChecked(index int, checked bool) error
 }
+
+// SortOrder specifies the order by which items are sorted.
+type SortOrder int
+
+const (
+	// SortAscending specifies ascending sort order.
+	SortAscending SortOrder = iota
+	
+	// SortDescending specifies descending sort order.
+	SortDescending
+)
+
+// Sorter is the interface that a model must implement to support sorting with a
+// widget like TableView.
+type Sorter interface {
+	// ColumnSortable returns whether column col is sortable.
+	ColumnSortable(col int) bool
+
+	// Sort sorts column col in order order.
+	//
+	// If col is -1 then no column is to be sorted. Sort must publish the event
+	// returned from SortChanged() after sorting.
+	Sort(col int, order SortOrder) error
+
+	// SortChanged returns an event that is published after sorting.
+	SortChanged() *Event
+
+	// SortedColumn returns the index of the currently sorted column, or -1 if
+	// no column is currently sorted.
+	SortedColumn() int
+
+	// SortOrder returns the current sort order.
+	SortOrder() SortOrder
+}
+
+// SorterBase implements the Sorter interface.
+//
+// You still need to provide your own implementation of at least the Sort method
+// to actually sort and reset the model. Your Sort method should call the
+// SorterBase implementation so the SortChanged event, that e.g. a TableView
+// widget depends on, is published.
+type SorterBase struct {
+	changedPublisher EventPublisher
+	col              int
+	order            SortOrder
+}
+
+func (sb *SorterBase) ColumnSortable(col int) bool {
+	return true
+}
+
+func (sb *SorterBase) Sort(col int, order SortOrder) error {
+	sb.col, sb.order = col, order
+
+	sb.changedPublisher.Publish()
+
+	return nil
+}
+
+func (sb *SorterBase) SortChanged() *Event {
+	return sb.changedPublisher.Event()
+}
+
+func (sb *SorterBase) SortedColumn() int {
+	return sb.col
+}
+
+func (sb *SorterBase) SortOrder() SortOrder {
+	return sb.order
+}
