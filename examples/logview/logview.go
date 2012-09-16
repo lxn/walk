81ef84be1c841428b086be94f7d6934fee95f419
zhubicen <zhubicen@gmail.com>
2012-09-16 08:25:11 +0800

    add one customize widget

--- examples/logview/logview.go
+++ examples/logview/logview.go
@@ -1,41 +1,101 @@
 // Copyright 2012 The Walk Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
-
 package main
 
 import (
-	//"fmt"
-	//"os"
-	//"strings"
-	"log"
-	//"time"
+	"errors"
+	"unsafe"
+	"syscall"
 )
 
-import "github.com/lxn/walk"
+import . "github.com/lxn/go-winapi"
+import (
+	"github.com/lxn/walk"
+)
 
-func main() {
-	walk.Initialize(walk.InitParams{PanicOnError: true})
-	defer walk.Shutdown()
+type LogView struct {
+	walk.WidgetBase
+	logChan chan string
+}
 
-	myWindow, _ := walk.NewMainWindow()
+func NewLogView(parent walk.Container) (*LogView, error) {
+	lc := make(chan string, 1024)
+	te := &LogView{logChan:lc}
 
-	myWindow.SetLayout(walk.NewVBoxLayout())
-	myWindow.SetTitle("LogView example")
+	if err := walk.InitChildWidget(
+		te,
+		parent,
+		"EDIT",
+		WS_TABSTOP|WS_VISIBLE|WS_VSCROLL|ES_MULTILINE|ES_WANTRETURN,
+		WS_EX_CLIENTEDGE); err != nil {
+		return nil, err
+	}
+	te.setReadOnly(true)
+	SendMessage(te.Handle(), EM_SETLIMITTEXT, 4294967295, 0)
+	return te, nil
+}
 
-	
-	logView, _ := walk.NewLogView(myWindow)
-	logView.PostAppendText("XXX")
-	log.SetOutput(logView)
-	
-	go func(){
-		for i := 0; i < 10000; i++{
-			log.Println("Text" + "\r\n")
+func (*LogView) LayoutFlags() walk.LayoutFlags {
+	return walk.ShrinkableHorz | walk.ShrinkableVert | walk.GrowableHorz | walk.GrowableVert | walk.GreedyHorz | walk.GreedyVert
+}
+
+func (te *LogView) MinSizeHint() walk.Size {
+	return walk.Size{20, 12}
+}
+
+func (te *LogView) SizeHint() walk.Size {
+	return walk.Size{100, 100}
+}
+
+func (te *LogView) setTextSelection(start, end int) {
+	SendMessage(te.Handle(), EM_SETSEL, uintptr(start), uintptr(end))
+}
+
+func (te *LogView) textLength() int{
+	return int(SendMessage(te.Handle(), 0x000E, uintptr(0), uintptr(0)))
+}
+
+func (te *LogView) AppendText(value string) {
+	textLength := te.textLength()
+	te.setTextSelection(textLength, textLength)
+	SendMessage(te.Handle(), EM_REPLACESEL, 0, uintptr(unsafe.Pointer(syscall.StringToUTF16Ptr(value))))
+}
+
+func (te *LogView) setReadOnly(readOnly bool) error {
+	if 0 == SendMessage(te.Handle(), EM_SETREADONLY, uintptr(BoolToBOOL(readOnly)), 0) {
+		return errors.New("fail to call EM_SETREADONLY")
+	}
+
+	return nil
+}
+
+func (te *LogView) PostAppendText(value string){
+	te.logChan <- value
+	PostMessage(te.Handle(), TEM_APPENDTEXT, 0, 0)
+}
+
+func (te *LogView)  Write(p []byte)(int, error){
+	te.PostAppendText(string(p) + "\r\n")
+	return len(p), nil
+}
+
+func (te *LogView) WndProc(hwnd HWND, msg uint32, wParam, lParam uintptr) uintptr {
+	switch msg {
+	case WM_GETDLGCODE:
+		if wParam == VK_RETURN {
+			return DLGC_WANTALLKEYS
 		}
-	}()
-	
-	myWindow.Show()
-	myWindow.SetMinMaxSize(walk.Size{320, 240}, walk.Size{})
-	myWindow.SetSize(walk.Size{400, 500})
-	myWindow.Run()
+
+		return DLGC_HASSETSEL | DLGC_WANTARROWS | DLGC_WANTCHARS
+	case TEM_APPENDTEXT:
+		select {
+		case value := <- te.logChan:
+			te.AppendText(value)
+		default:
+			return 0
+		}
+	}
+
+	return te.WidgetBase.WndProc(hwnd, msg, wParam, lParam)
 }
---
+++ examples/logview/logviewapp.go
@@ -0,0 +1,42 @@
+// Copyright 2012 The Walk Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package main
+
+import (
+	//"fmt"
+	//"os"
+	//"strings"
+	"log"
+	"time"
+)
+
+import "github.com/lxn/walk"
+
+func main() {
+	walk.Initialize(walk.InitParams{PanicOnError: true})
+	defer walk.Shutdown()
+
+	myWindow, _ := walk.NewMainWindow()
+
+	myWindow.SetLayout(walk.NewVBoxLayout())
+	myWindow.SetTitle("LogView example")
+
+	
+	logView, _ := NewLogView(myWindow)
+	logView.PostAppendText("XXX")
+	log.SetOutput(logView)
+	
+	go func(){
+		for i := 0; i < 10000; i++{
+			time.Sleep(100*time.Millisecond)
+			log.Println("Text" + "\r\n")
+		}
+	}()
+	
+	myWindow.Show()
+	myWindow.SetMinMaxSize(walk.Size{320, 240}, walk.Size{})
+	myWindow.SetSize(walk.Size{400, 500})
+	myWindow.Run()
+}
