db5b616548d6874c5edbba51736de52e0096308d
Alexander Neumann <an2048@gmail.com>
2012-09-29 14:23:23 +0200

    declarative: Add GroupBox

---
+++ declarative/groupbox.go
@@ -0,0 +1,54 @@
+// Copyright 2012 The Walk Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package declarative
+
+import (
+	"github.com/lxn/walk"
+)
+
+type GroupBox struct {
+	Widget        **walk.GroupBox
+	Name          string
+	StretchFactor int
+	Row           int
+	RowSpan       int
+	Column        int
+	ColumnSpan    int
+	Font          Font
+	Title         string
+	Layout        Layout
+	Children      []Widget
+}
+
+func (gb GroupBox) Create(parent walk.Container) error {
+	w, err := walk.NewGroupBox(parent)
+	if err != nil {
+		return err
+	}
+
+	return InitWidget(gb, w, func() error {
+		if err := w.SetTitle(gb.Title); err != nil {
+			return err
+		}
+
+		if gb.Widget != nil {
+			*gb.Widget = w
+		}
+
+		return nil
+	})
+}
+
+func (gb GroupBox) CommonInfo() (name string, stretchFactor, row, rowSpan, column, columnSpan int) {
+	return gb.Name, gb.StretchFactor, gb.Row, gb.RowSpan, gb.Column, gb.ColumnSpan
+}
+
+func (gb GroupBox) Font_() *Font {
+	return &gb.Font
+}
+
+func (gb GroupBox) ContainerInfo() (Layout, []Widget) {
+	return gb.Layout, gb.Children
+}
