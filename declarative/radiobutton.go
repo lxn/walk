04e18fe5fc5b3a79b740080223cf6c16f1b428a4
Alexander Neumann <an2048@gmail.com>
2012-09-29 14:56:38 +0200

    declarative: Add RadioButton

---
+++ declarative/radiobutton.go
@@ -0,0 +1,53 @@
+// Copyright 2012 The Walk Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package declarative
+
+import (
+	"github.com/lxn/walk"
+)
+
+type RadioButton struct {
+	Widget        **walk.RadioButton
+	Name          string
+	StretchFactor int
+	Row           int
+	RowSpan       int
+	Column        int
+	ColumnSpan    int
+	Font          Font
+	Text          string
+	OnClicked     walk.EventHandler
+}
+
+func (rb RadioButton) Create(parent walk.Container) error {
+	w, err := walk.NewRadioButton(parent)
+	if err != nil {
+		return err
+	}
+
+	return InitWidget(rb, w, func() error {
+		if err := w.SetText(rb.Text); err != nil {
+			return err
+		}
+
+		if rb.OnClicked != nil {
+			w.Clicked().Attach(rb.OnClicked)
+		}
+
+		if rb.Widget != nil {
+			*rb.Widget = w
+		}
+
+		return nil
+	})
+}
+
+func (rb RadioButton) CommonInfo() (name string, stretchFactor, row, rowSpan, column, columnSpan int) {
+	return rb.Name, rb.StretchFactor, rb.Row, rb.RowSpan, rb.Column, rb.ColumnSpan
+}
+
+func (rb RadioButton) Font_() *Font {
+	return &rb.Font
+}
