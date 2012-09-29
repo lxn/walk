1f4af986c30ae0c089add750fca2f5caa6f3632b
Alexander Neumann <an2048@gmail.com>
2012-09-29 14:16:09 +0200

    declarative: Add Dialog

---
+++ declarative/dialog.go
@@ -0,0 +1,67 @@
+// Copyright 2012 The Walk Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package declarative
+
+import (
+	"github.com/lxn/walk"
+)
+
+type Dialog struct {
+	Widget        **walk.Dialog
+	Name          string
+	Font          Font
+	Title         string
+	Layout        Layout
+	Children      []Widget
+	DefaultButton **walk.PushButton
+	CancelButton  **walk.PushButton
+}
+
+func (d Dialog) Create(parent walk.Container) error {
+	var owner walk.RootWidget
+	if o, ok := parent.(walk.RootWidget); ok {
+		owner = o
+	}
+
+	w, err := walk.NewDialog(owner)
+	if err != nil {
+		return err
+	}
+
+	return InitWidget(d, w, func() error {
+		if err := w.SetTitle(d.Title); err != nil {
+			return err
+		}
+
+		if d.DefaultButton != nil {
+			if err := w.SetDefaultButton(*d.DefaultButton); err != nil {
+				return err
+			}
+		}
+		if d.CancelButton != nil {
+			if err := w.SetCancelButton(*d.CancelButton); err != nil {
+				return err
+			}
+		}
+
+		if d.Widget != nil {
+			*d.Widget = w
+		}
+
+		return nil
+	})
+}
+
+func (d Dialog) CommonInfo() (name string, stretchFactor, row, rowSpan, column, columnSpan int) {
+	return d.Name, 0, 0, 0, 0, 0
+}
+
+func (d Dialog) Font_() *Font {
+	return &d.Font
+}
+
+func (d Dialog) ContainerInfo() (Layout, []Widget) {
+	return d.Layout, d.Children
+}
