lxn/walk

commit db77afa93243846fb326f829180674cce9432ed2

Take measures against resource leaks.

window.go
@@ -24,6 +24,10 @@ const (
 
 // Window is an interface that provides operations common to all windows.
 type Window interface {
+	// AddDisposable adds a Disposable resource that should be disposed of
+	// together with this Window.
+	AddDisposable(d Disposable)
+
 	// AsWindowBase returns a *WindowBase, a pointer to an instance of the
 	// struct that implements most operations common to all windows.
 	AsWindowBase() *WindowBase
@@ -72,6 +76,10 @@ type Window interface {
 	// Also, if a Container is disposed of, all its descendants will be released
 	// as well.
 	Dispose()
+
+	// Disposing returns an Event that is published when the Window is disposed
+	// of.
+	Disposing() *Event
 
 	// Enabled returns if the Window is enabled for user interaction.
 	Enabled() bool
@@ -256,6 +264,8 @@ type WindowBase struct {
 	name                    string
 	font                    *Font
 	contextMenu             *Menu
+	disposables             []Disposable
+	disposingPublisher      EventPublisher
 	keyDownPublisher        KeyEventPublisher
 	keyPressPublisher       KeyEventPublisher
 	keyUpPublisher          KeyEventPublisher
@@ -553,6 +563,12 @@ func (wb *WindowBase) AsWindowBase() *WindowBase {
 	return wb
 }
 
+// AddDisposable adds a Disposable resource that should be disposed of
+// together with this Window.
+func (wb *WindowBase) AddDisposable(d Disposable) {
+	wb.disposables = append(wb.disposables, d)
+}
+
 // Dispose releases the operating system resources, associated with the
 // *WindowBase.
 //
@@ -560,8 +576,14 @@ func (wb *WindowBase) AsWindowBase() *WindowBase {
 // Also, if a Container is disposed of, all its descendants will be released
 // as well.
 func (wb *WindowBase) Dispose() {
+	for _, d := range wb.disposables {
+		d.Dispose()
+	}
+
 	hWnd := wb.hWnd
 	if hWnd != 0 {
+		wb.disposingPublisher.Publish()
+
 		switch w := wb.window.(type) {
 		case *ToolTip:
 		case Widget:
@@ -569,16 +591,25 @@ func (wb *WindowBase) Dispose() {
 		}
 
 		wb.hWnd = 0
-		win.DestroyWindow(hWnd)
+		if _, ok := hwnd2WindowBase[hWnd]; ok {
+			win.DestroyWindow(hWnd)
+		}
 	}
 
 	if cm := wb.contextMenu; cm != nil {
 		cm.actions.Clear()
+		cm.Dispose()
 	}
 
 	for _, p := range wb.name2Property {
 		p.SetSource(nil)
 	}
+}
+
+// Disposing returns an Event that is published when the Window is disposed
+// of.
+func (wb *WindowBase) Disposing() *Event {
+	return wb.disposingPublisher.Event()
 }
 
 // IsDisposed returns if the *WindowBase has been disposed of.
@@ -1292,23 +1323,22 @@ func (wb *WindowBase) WndProc(hwnd win.HWND, msg uint32, wParam, lParam uintptr)
 		wb.sizeChangedPublisher.Publish()
 
 	case win.WM_DESTROY:
-		switch w := wb.window.(type) {
-		case *ToolTip:
-		case Widget:
-			globalToolTip.RemoveTool(w)
+		if wb.origWndProcPtr != 0 {
+			// As we subclass all windows of system classes, we prevented the
+			// clean-up code in the WM_NCDESTROY handlers of some windows from
+			// being called. To fix this, we restore the original window
+			// procedure here.
+			win.SetWindowLongPtr(wb.hWnd, win.GWLP_WNDPROC, wb.origWndProcPtr)
 		}
 
 		delete(hwnd2WindowBase, hwnd)
 
-		wb.hWnd = 0
 		wb.window.Dispose()
+		wb.hWnd = 0
 	}
 
-	if window := windowFromHandle(hwnd); window != nil {
-		origWndProcPtr := window.AsWindowBase().origWndProcPtr
-		if origWndProcPtr != 0 {
-			return win.CallWindowProc(origWndProcPtr, hwnd, msg, wParam, lParam)
-		}
+	if wb.origWndProcPtr != 0 {
+		return win.CallWindowProc(wb.origWndProcPtr, hwnd, msg, wParam, lParam)
 	}
 
 	return win.DefWindowProc(hwnd, msg, wParam, lParam)

action.go
@@ -80,6 +80,7 @@ func (a *Action) release() {
 
 		if a.menu != nil {
 			a.menu.actions.Clear()
+			a.menu.Dispose()
 		}
 
 		delete(actionsById, a.id)

declarative/action.go
@@ -180,7 +180,7 @@ func addToActionList(list *walk.ActionList, actions []*walk.Action) error {
 }
 
 func setActionImage(action *walk.Action, image interface{}) (err error) {
-	var img *walk.Bitmap
+	var img walk.Image
 
 	switch image := image.(type) {
 	case nil:
@@ -190,13 +190,14 @@ func setActionImage(action *walk.Action, image interface{}) (err error) {
 		img = image
 
 	case string:
-		if img, err = walk.NewBitmapFromFile(image); err != nil {
+		if img, err = imageFromFile(image); err != nil {
 			return
 		}
-
-	default:
-		return errors.New("invalid type for Image")
 	}
 
-	return action.SetImage(img)
+	if bmp, ok := img.(*walk.Bitmap); ok {
+		return action.SetImage(bmp)
+	}
+
+	return errors.New("invalid type for Image")
 }

declarative/builder.go
@@ -14,7 +14,25 @@ import (
 	"github.com/lxn/walk"
 )
 
-var conditionsByName = make(map[string]walk.Condition)
+var (
+	conditionsByName = make(map[string]walk.Condition)
+	imagesByFilePath = make(map[string]walk.Image)
+)
+
+func imageFromFile(filePath string) (walk.Image, error) {
+	if image, ok := imagesByFilePath[filePath]; ok {
+		return image, nil
+	}
+
+	image, err := walk.NewImageFromFile(filePath)
+	if err != nil {
+		return nil, err
+	}
+
+	imagesByFilePath[filePath] = image
+
+	return image, nil
+}
 
 func MustRegisterCondition(name string, condition walk.Condition) {
 	if name == "" {

declarative/pushbutton.go
@@ -47,7 +47,7 @@ func (pb PushButton) Create(builder *Builder) error {
 		img := pb.Image
 		if s, ok := img.(string); ok {
 			var err error
-			if img, err = walk.NewImageFromFile(s); err != nil {
+			if img, err = imageFromFile(s); err != nil {
 				return err
 			}
 		}

declarative/toolbutton.go
@@ -46,7 +46,7 @@ func (tb ToolButton) Create(builder *Builder) error {
 		img := tb.Image
 		if s, ok := img.(string); ok {
 			var err error
-			if img, err = walk.NewImageFromFile(s); err != nil {
+			if img, err = imageFromFile(s); err != nil {
 				return err
 			}
 		}

tableview.go
@@ -140,9 +140,7 @@ func NewTableView(parent Container) (*TableView, error) {
 func (tv *TableView) Dispose() {
 	tv.columns.unsetColumnsTV()
 
-	if tv.model != nil {
-		tv.detachModel()
-	}
+	tv.SetModel(nil)
 
 	if tv.hWnd != 0 {
 		if !win.KillTimer(tv.hWnd, tableViewCurrentIndexChangedTimerId) {

commit 9481b0dc1b832cf2cb6d0979ddb47acc8d6f9044

Fix bug introduced in db77afa

window.go
@@ -1278,6 +1278,8 @@ func (wb *WindowBase) handleKeyUp(wParam, lParam uintptr) {
 // When implementing your own WndProc to add or modify behavior, call the
 // WndProc of the embedded window for messages you don't handle yourself.
 func (wb *WindowBase) WndProc(hwnd win.HWND, msg uint32, wParam, lParam uintptr) uintptr {
+	window := windowFromHandle(hwnd)
+
 	switch msg {
 	case win.WM_ERASEBKGND:
 		if wb.background == nil {
@@ -1376,8 +1378,10 @@ func (wb *WindowBase) WndProc(hwnd win.HWND, msg uint32, wParam, lParam uintptr)
 		wb.hWnd = 0
 	}
 
-	if wb.origWndProcPtr != 0 {
-		return win.CallWindowProc(wb.origWndProcPtr, hwnd, msg, wParam, lParam)
+	if window != nil {
+		if wndProc := window.AsWindowBase().origWndProcPtr; wndProc != 0 {
+			return win.CallWindowProc(wndProc, hwnd, msg, wParam, lParam)
+		}
 	}
 
 	return win.DefWindowProc(hwnd, msg, wParam, lParam)