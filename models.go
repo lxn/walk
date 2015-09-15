cff91fbf85bb4d33ad13fe624bcff872ba06ca92
Joseph Watson <jwatson@genisyscu.org>
2015-09-15 12:59:53 -0400

    Add build tags to allow this library to be included in a cross platform program.  This allows for several things: 1) Develop on something other then a windows box, and cross compile the program to target windows. 2) Develop a program that will run on the command line for any OS, but also have a gui when run on windows. 3) Use the walk library for the windows gui, and some other library for a gui on other platforms.

--- models.go
+++ models.go
@@ -2,6 +2,8 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
+// +build windows
+
 package walk
 
 // BindingValueProvider is the interface that a model must implement to support
