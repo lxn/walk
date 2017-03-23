2f8d6194fd1f98469c43662125fe2a6fe116c7b7
Alexander Neumann <alexander.neumann@picos-software.com>
2017-03-23 17:02:24 +0100

    Add Separator widget

---
+++ declarative/separator.go
@@ -0,0 +1,99 @@
+// Copyright 2017 The Walk Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// +build windows
+
+package declarative
+
+import (
+	"github.com/lxn/walk"
+)
+
+type HSeparator struct {
+	AssignTo           **walk.Separator
+	Name               string
+	Enabled            Property
+	Visible            Property
+	Font               Font
+	ToolTipText        Property
+	MinSize            Size
+	MaxSize            Size
+	StretchFactor      int
+	Row                int
+	RowSpan            int
+	Column             int
+	ColumnSpan         int
+	AlwaysConsumeSpace bool
+	ContextMenuItems   []MenuItem
+	OnKeyDown          walk.KeyEventHandler
+	OnKeyPress         walk.KeyEventHandler
+	OnKeyUp            walk.KeyEventHandler
+	OnMouseDown        walk.MouseEventHandler
+	OnMouseMove        walk.MouseEventHandler
+	OnMouseUp          walk.MouseEventHandler
+	OnSizeChanged      walk.EventHandler
+}
+
+func (s HSeparator) Create(builder *Builder) error {
+	w, err := walk.NewHSeparator(builder.Parent())
+	if err != nil {
+		return err
+	}
+
+	return builder.InitWidget(s, w, func() error {
+		if s.AssignTo != nil {
+			*s.AssignTo = w
+		}
+
+		return nil
+	})
+}
+
+func (w HSeparator) WidgetInfo() (name string, disabled, hidden bool, font *Font, toolTipText string, minSize, maxSize Size, stretchFactor, row, rowSpan, column, columnSpan int, alwaysConsumeSpace bool, contextMenuItems []MenuItem, OnKeyDown walk.KeyEventHandler, OnKeyPress walk.KeyEventHandler, OnKeyUp walk.KeyEventHandler, OnMouseDown walk.MouseEventHandler, OnMouseMove walk.MouseEventHandler, OnMouseUp walk.MouseEventHandler, OnSizeChanged walk.EventHandler) {
+	return w.Name, false, false, &w.Font, "", w.MinSize, w.MaxSize, w.StretchFactor, w.Row, w.RowSpan, w.Column, w.ColumnSpan, w.AlwaysConsumeSpace, w.ContextMenuItems, w.OnKeyDown, w.OnKeyPress, w.OnKeyUp, w.OnMouseDown, w.OnMouseMove, w.OnMouseUp, w.OnSizeChanged
+}
+
+type VSeparator struct {
+	AssignTo           **walk.Separator
+	Name               string
+	Enabled            Property
+	Visible            Property
+	Font               Font
+	ToolTipText        Property
+	MinSize            Size
+	MaxSize            Size
+	StretchFactor      int
+	Row                int
+	RowSpan            int
+	Column             int
+	ColumnSpan         int
+	AlwaysConsumeSpace bool
+	ContextMenuItems   []MenuItem
+	OnKeyDown          walk.KeyEventHandler
+	OnKeyPress         walk.KeyEventHandler
+	OnKeyUp            walk.KeyEventHandler
+	OnMouseDown        walk.MouseEventHandler
+	OnMouseMove        walk.MouseEventHandler
+	OnMouseUp          walk.MouseEventHandler
+	OnSizeChanged      walk.EventHandler
+}
+
+func (s VSeparator) Create(builder *Builder) error {
+	w, err := walk.NewVSeparator(builder.Parent())
+	if err != nil {
+		return err
+	}
+
+	return builder.InitWidget(s, w, func() error {
+		if s.AssignTo != nil {
+			*s.AssignTo = w
+		}
+
+		return nil
+	})
+}
+
+func (w VSeparator) WidgetInfo() (name string, disabled, hidden bool, font *Font, toolTipText string, minSize, maxSize Size, stretchFactor, row, rowSpan, column, columnSpan int, alwaysConsumeSpace bool, contextMenuItems []MenuItem, OnKeyDown walk.KeyEventHandler, OnKeyPress walk.KeyEventHandler, OnKeyUp walk.KeyEventHandler, OnMouseDown walk.MouseEventHandler, OnMouseMove walk.MouseEventHandler, OnMouseUp walk.MouseEventHandler, OnSizeChanged walk.EventHandler) {
+	return w.Name, false, false, &w.Font, "", w.MinSize, w.MaxSize, w.StretchFactor, w.Row, w.RowSpan, w.Column, w.ColumnSpan, w.AlwaysConsumeSpace, w.ContextMenuItems, w.OnKeyDown, w.OnKeyPress, w.OnKeyUp, w.OnMouseDown, w.OnMouseMove, w.OnMouseUp, w.OnSizeChanged
+}
