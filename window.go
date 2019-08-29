026611184a72972dd942f8d46d5bf7a4a863b884
Alexander Neumann <alexander.neumann@picos-software.com>
2019-08-29 17:03:29 +0200

    Fix border painting glitch with graphics effects over remote desktop
    Widgets like LineEdit, NumberEdit, TextEdit and TableView were painted without borders and scroll bars after minimizing the remote desktop window and then bringing it up again.

--- window.go
+++ window.go
@@ -2228,7 +2228,10 @@ func (wb *WindowBase) WndProc(hwnd win.HWND, msg uint32, wParam, lParam uintptr)
 					}
 				}
 			}
-			wnd.AsWidgetBase().invalidateBorderInParent()
+
+			if wb.Form() == appSingleton.ActiveForm() {
+				wnd.AsWidgetBase().invalidateBorderInParent()
+			}
 		}
 
 		wb.focusedChangedPublisher.Publish()
