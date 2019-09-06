087ed651f548fdfe96f8cd973b666262f2bdb801
Joshua Sjoding <joshua.sjoding@scjalliance.com>
2019-09-06 00:04:15 -0700

    Removed thread safety from WindowGroup
    A WindowGroup should only ever by accessed by a single thread. Inclusion
    of a mutex and an atomic counter made it needlessly complicated.

--- windowgroup.go
+++ windowgroup.go
@@ -8,7 +8,6 @@ package walk
 
 import (
 	"sync"
-	"sync/atomic"
 )
 
 // The global window group manager instance.
@@ -73,14 +72,12 @@ func (m *windowGroupManager) removeGroup(threadID uint32) {
 // the group. When the number of references reaches zero, the
 // group is disposed of.
 type WindowGroup struct {
-	counter    windowGroupCounter // Accessed atomically, keep at front for proper alignment
-	completion func(uint32)       // Called from dispose()
+	refs       int // Tracks the number of windows that rely on this group
+	ignored    int // Tracks the number of refs created by the group itself
 	threadID   uint32
-
-	removed bool // Has the group been removed from its manager? (used for race detection)
-
-	mutex   sync.RWMutex
-	toolTip *ToolTip
+	completion func(uint32) // Used to tell the window group manager to remove this group
+	removed    bool         // Has this group been removed from its manager? (used for race detection)
+	toolTip    *ToolTip
 }
 
 // newWindowGroup returns a new window group for the given thread ID.
@@ -101,8 +98,7 @@ func (g *WindowGroup) ThreadID() uint32 {
 
 // Refs returns the current number of references to the group.
 func (g *WindowGroup) Refs() int {
-	refs, _ := g.counter.Value()
-	return refs
+	return g.refs
 }
 
 // Add changes the group's reference counter by delta, which may be negative.
@@ -111,12 +107,58 @@ func (g *WindowGroup) Refs() int {
 //
 // If the reference counter goes negative Add will panic.
 func (g *WindowGroup) Add(delta int) {
-	g.add(delta, 0)
+	if g.removed {
+		panic("walk: add() called on a WindowGroup that has been removed from its manager")
+	}
+
+	g.refs += delta
+	//fmt.Printf("Thread %d: Refs: %d, Ignored: %d\n", g.threadID, g.refs, g.ignored)
+	if g.refs < 0 {
+		panic("walk: negative WindowGroup refs counter")
+	}
+	if g.refs-g.ignored == 0 {
+		g.dispose()
+	}
 }
 
 // Done decrements the group's reference counter by one.
 func (g *WindowGroup) Done() {
 	g.Add(-1)
+}
+
+// ToolTip returns the tool tip control for the group, if one exists.
+func (g *WindowGroup) ToolTip() *ToolTip {
+	return g.toolTip
+}
+
+// CreateToolTip returns a tool tip control for the group.
+//
+// If a control has not already been prepared for the group one will be
+// created.
+func (g *WindowGroup) CreateToolTip() (*ToolTip, error) {
+	if g.toolTip != nil {
+		return g.toolTip, nil
+	}
+
+	tt, err := NewToolTip() // This must not call group.ToolTip()
+	if err != nil {
+		return nil, err
+	}
+	g.toolTip = tt
+
+	// At this point the ToolTip has already added a reference for itself
+	// to the group as part of the ToolTip's InitWindow process. We don't
+	// want it to count toward the group's liveness, however, because it
+	// would keep the group from cleaning up after itself.
+	//
+	// To solve this problem we also keep track of the number of
+	// references that each group should ignore. The ignored references
+	// are subtracted from the total number of references when evaluating
+	// liveness. The expectation is that ignored references will be
+	// removed as part of the group's disposal process.
+	g.ignore(1)
+
+	return tt, nil
 }
 
 // ignore changes the number of references that the group will ignore.
@@ -125,88 +167,18 @@ func (g *WindowGroup) Done() {
 // of references created by the group itself. When finished with a group,
 // call Done() instead.
 func (g *WindowGroup) ignore(delta int) {
-	g.add(0, delta)
-}
-
-func (g *WindowGroup) add(refs, ignored int) {
-	// The use of an atomic counter here is theoretically unnecessary because
-	// the caller should always be calling this from the same thread. The
-	// thread-safe counter is used out of an abundance of caution.
-
-	// Best-effort race detection in case wgm.Group() is called while the
-	// group is being disposed.
 	if g.removed {
-		panic("walk: add() called on a WindowGroup that has been removed from its manager")
+		panic("walk: ignore() called on a WindowGroup that has been removed from its manager")
 	}
 
-	refs, ignored = g.counter.Add(refs, ignored)
+	g.ignored += delta
 	//fmt.Printf("Thread %d: Refs: %d, Ignored: %d\n", g.threadID, refs, ignored)
-	if refs < 0 {
-		panic("walk: negative WindowGroup refs counter")
-	}
-	if ignored < 0 {
+	if g.ignored < 0 {
 		panic("walk: negative WindowGroup ignored counter")
 	}
-	if refs-ignored == 0 {
+	if g.refs-g.ignored == 0 {
 		g.dispose()
-		g.removed = true // race detection only
-		g.completion(g.threadID)
 	}
-}
-
-// ToolTip returns the tool tip control for the group, if one exists.
-func (g *WindowGroup) ToolTip() *ToolTip {
-	g.mutex.RLock()
-	tt := g.toolTip
-	g.mutex.RUnlock()
-	return tt
-}
-
-// CreateToolTip returns a tool tip control for the group.
-//
-// If a control has not already been prepared for the group one will be
-// created.
-func (g *WindowGroup) CreateToolTip() (*ToolTip, error) {
-	// The use of a mutex here is theoretically unnecessary because the
-	// caller should always be calling this from the same thread. The mutex
-	// is used out of an abundance of caution and may be removed in the
-	// future.
-
-	// Fast path with read lock
-	g.mutex.RLock()
-	if tt := g.toolTip; tt != nil {
-		g.mutex.RUnlock()
-		return tt, nil
-	}
-	g.mutex.RUnlock()
-
-	// Slow path with write lock
-	g.mutex.Lock()
-	if tt := g.toolTip; tt != nil {
-		g.mutex.Unlock()
-		return tt, nil
-	}
-
-	tt, err := NewToolTip() // This must not call group.ToolTip()
-	if err == nil {
-		g.toolTip = tt
-
-		// At this point the ToolTip has already added a reference for itself
-		// to the group as part of the ToolTip's InitWindow process. We don't
-		// want it to count toward the group's liveness, however, because it
-		// would keep the group from cleaning up after itself.
-		//
-		// To solve this problem we also keep track of the number of
-		// references that each group should ignore. The ignored references
-		// are subtracted from the total number of references when evaluating
-		// liveness. The expectation is that ignored references will be
-		// removed as part of the group's disposal process.
-		g.ignore(1)
-	}
-
-	g.mutex.Unlock()
-
-	return tt, err
 }
 
 // dispose releases any resources consumed by the group.
@@ -216,40 +188,6 @@ func (g *WindowGroup) dispose() {
 		g.toolTip.Dispose()
 		g.toolTip = nil
 	}
-}
-
-// windowGroupCounter is an atomic counter that stores two int32 values
-// within a single uint64 state. It stores a pair of integers that are
-// accessed atomically.
-//
-// Care must be taken to ensure the counter has 64-bit alignment even on
-// 32-bit systems. This can be accomplished by making it the first member
-// of its containing struct.
-type windowGroupCounter uint64
-
-// Add changes the counter by the given deltas, which may be negative.
-func (wgc *windowGroupCounter) Add(refs, ignored int) (newRefs, newIgnored int) {
-	// Constrain individual deltas to 32-bits
-	dr := int32(refs)
-	di := int32(ignored)
-
-	// Pack into a single uint64 delta
-	delta := uint64(uint64(dr)<<32 | uint64(di))
-
-	// Atomic add
-	addr := (*uint64)(wgc)
-	state := atomic.AddUint64(addr, delta)
-
-	// Unpack state
-	return int(int32(state >> 32)), int(int32(state))
-}
-
-// Value returns the current value of the counter.
-func (wgc *windowGroupCounter) Value() (refs, ignored int) {
-	// Atomic load
-	addr := (*uint64)(wgc)
-	state := atomic.LoadUint64(addr)
-
-	// Unpack state
-	return int(int32(state >> 32)), int(int32(state))
+	g.removed = true // race detection only
+	g.completion(g.threadID)
 }
