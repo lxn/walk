b0816ea147f2376b29a16999fea51e76e4774e35
borey <booxlp@gmail.com>
2019-11-27 12:04:59 +0800

    Update logview.go
    fixed logview example

--- examples/logview/logview.go
+++ examples/logview/logview.go
@@ -4,19 +4,14 @@
 package main
 
 import (
-	"errors"
-	"syscall"
-	"unsafe"
-)
-
-import (
 	"github.com/lxn/walk"
 	"github.com/lxn/win"
 )
 
 type LogView struct {
-	walk.WidgetBase
+	walk.TextEdit
 	logChan chan string
+	maxSize int
 }
 
 const (
@@ -24,46 +19,29 @@ const (
 )
 
 func NewLogView(parent walk.Container) (*LogView, error) {
-	lc := make(chan string, 1024)
-	lv := &LogView{logChan: lc}
-
-	if err := walk.InitWidget(
-		lv,
+	lv := &LogView{
+		logChan: make(chan string, 1024),
+		maxSize: 0x7FFF0000,
+	}
+	if err := walk.InitWidget(lv,
 		parent,
 		"EDIT",
 		win.WS_TABSTOP|win.WS_VISIBLE|win.WS_VSCROLL|win.ES_MULTILINE|win.ES_WANTRETURN,
 		win.WS_EX_CLIENTEDGE); err != nil {
 		return nil, err
 	}
-	lv.setReadOnly(true)
-	lv.SendMessage(win.EM_SETLIMITTEXT, 4294967295, 0)
+	lv.TextEdit.SetReadOnly(true)
+	lv.TextEdit.SetMaxLength(lv.maxSize)
 	return lv, nil
 }
 
-func (*LogView) CreateLayoutItem(ctx *walk.LayoutContext) walk.LayoutItem {
-	return walk.NewGreedyLayoutItem()
-}
-
-func (lv *LogView) setTextSelection(start, end int) {
-	lv.SendMessage(win.EM_SETSEL, uintptr(start), uintptr(end))
-}
-
-func (lv *LogView) textLength() int {
-	return int(lv.SendMessage(0x000E, uintptr(0), uintptr(0)))
-}
-
 func (lv *LogView) AppendText(value string) {
-	textLength := lv.textLength()
-	lv.setTextSelection(textLength, textLength)
-	lv.SendMessage(win.EM_REPLACESEL, 0, uintptr(unsafe.Pointer(syscall.StringToUTF16Ptr(value))))
-}
-
-func (lv *LogView) setReadOnly(readOnly bool) error {
-	if 0 == lv.SendMessage(win.EM_SETREADONLY, uintptr(win.BoolToBOOL(readOnly)), 0) {
-		return errors.New("fail to call EM_SETREADONLY")
+	textLength := lv.TextEdit.TextLength()
+	if textLength+len(value) < lv.maxSize {
+		lv.TextEdit.AppendText(value)
+	} else {
+		lv.TextEdit.SetText(value)
 	}
-
-	return nil
 }
 
 func (lv *LogView) PostAppendText(value string) {
@@ -78,20 +56,12 @@ func (lv *LogView) Write(p []byte) (int, error) {
 
 func (lv *LogView) WndProc(hwnd win.HWND, msg uint32, wParam, lParam uintptr) uintptr {
 	switch msg {
-	case win.WM_GETDLGCODE:
-		if wParam == win.VK_RETURN {
-			return win.DLGC_WANTALLKEYS
-		}
-
-		return win.DLGC_HASSETSEL | win.DLGC_WANTARROWS | win.DLGC_WANTCHARS
 	case TEM_APPENDTEXT:
 		select {
 		case value := <-lv.logChan:
 			lv.AppendText(value)
 		default:
-			return 0
 		}
 	}
-
-	return lv.WidgetBase.WndProc(hwnd, msg, wParam, lParam)
+	return lv.TextEdit.WndProc(hwnd, msg, wParam, lParam)
 }
