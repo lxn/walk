4bac4c4d42f71487b59d1021d8091030209a9bee
kukymbr <sergey@espressio.ru>
2023-10-16 12:01:30 +0300

    add RunWithContext function

--- mainloop_default.go
+++ mainloop_default.go
@@ -2,21 +2,27 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
+//go:build windows && !walk_use_cgo
 // +build windows,!walk_use_cgo
 
 package walk
 
 import (
+	"context"
 	"unsafe"
 
 	"github.com/lxn/win"
 )
 
-func (fb *FormBase) mainLoop() int {
+func (fb *FormBase) mainLoop(ctx context.Context) int {
 	msg := (*win.MSG)(unsafe.Pointer(win.GlobalAlloc(0, unsafe.Sizeof(win.MSG{}))))
 	defer win.GlobalFree(win.HGLOBAL(unsafe.Pointer(msg)))
 
 	for fb.hWnd != 0 {
+		if err := ctx.Err(); err != nil {
+			return 0
+		}
+
 		switch win.GetMessage(msg, 0, 0, 0) {
 		case 0:
 			return int(msg.WParam)
